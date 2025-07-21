615f968640a4c315f1c64d33b15f94216bafdec9
CharanNaik47 <charannaik500@gmail.com>
2025-07-22 02:21:09 +0530

    Adhar File

---
+++ Aadhar.java
@@ -0,0 +1,27 @@
+class Aadhar{
+	
+ private String name;
+ private int age;
+ private String adrs;
+ 
+	 public void setName(String a){
+		 name=a;
+	 }
+	 public String getName(){
+		 return name;
+	 }
+	 
+	 public void setAge(int b){
+		 age=b;
+	 }
+	 public int getAge(){
+		 return age;
+	 }
+	 
+	 public void setAdrs(String c){
+		 adrs=c;
+	 }
+	 public String getAdrs(){
+		 return adrs;
+	 }
+}
