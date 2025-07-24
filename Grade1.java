46360410d1ca9f7b7da37c3483494ea2fdd26110
CharanNaik47 <charannaik500@gmail.com>
2025-07-24 11:45:56 +0530

    Whileloop(Grade1)

---
+++ Grade1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Grade1{
+
+public static void main(String[] args){
+	
+		Scanner sc= new Scanner(System.in);
+	int NoOf_Student=3;
+		
+	int i=1;
+	while(i<=NoOf_Student){
+		i++;
+		int percentage= sc.nextInt();
+		
+		if(percentage<0 || percentage >100){
+			System.out.println(percentage+" Invalid");
+		}
+			
+		else if(percentage>=90){
+			System.out.println(percentage+" Grade : A+");
+		}
+		else if(percentage>=80){
+			System.out.println(percentage+" Grade : A");
+		}
+		else if(percentage>=60){
+			System.out.println(percentage+" Grade : B");
+		}
+		else if(percentage>=35){
+			System.out.println(percentage+" Grade : C");
+		}
+		else{
+			System.out.println(percentage+" Fail");
+		}
+		
+}}}
